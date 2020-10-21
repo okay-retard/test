04dc6cb21de43fa7985a7275d12f54bdbfe2be0b
knocker1 <71930795+knocker1@users.noreply.github.com>
2020-10-21 13:29:27 +0530

    Update ytdl.py

--- SaitamaRobot/modules/ytdl.py
+++ SaitamaRobot/modules/ytdl.py
@@ -243,3 +243,19 @@ async def youtube_search(
         nexttok = "KeyError, try again."
         return (nexttok, videos)
 
+@telethn.on(events.NewMessage(pattern="^/song (.*)"))
+async def _(event):
+    song = url = event.pattern_match.group(1) + " " + "song"
+    if not song:
+        await event.reply("`Enter song name`")
+        return
+    await event.edit("Processing...") 
+    await event.reply("`Fetching data, please wait..`")
+    os.system(f"youtube-dl -x --audio-format mp3 --add-metadata --embed-thumbnai 'ytsearch:{song}'")
+    l = glob.glob("*.mp3")
+    if not l:
+        await event.edit("`Song not found`")
+        return
+    await event.client.send_file(event.chat_id, l, supports_streaming=True, reply_to=event.message)
+    #await reply.delete()
+    subprocess.check_output("rm -rf *.mp3",shell=True)
