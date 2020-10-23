f6081068961080bae7ada54a2e8b02b2b8e2aafb
knocker1 <71930795+knocker1@users.noreply.github.com>
2020-10-23 11:43:42 +0530

    Update ytdl.py

--- SaitamaRobot/modules/ytdl.py
+++ SaitamaRobot/modules/ytdl.py
@@ -168,7 +168,7 @@ async def download_video(v_url):
             sed.chat_id,
             f"{ytdl_data['id']}.mp4",
             supports_streaming=True,
-            caption=ytdl_data["title"+""+"okk"]
+            caption=ytdl_data["title"]
             #progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
              #   progress(
               #      d, t, v_url, c_time, "Uploading..", f"{ytdl_data['title']}.mp4"
